4d91737d7e485d54f96634f1d50d6fe78bfa604d
Madness <Madnessrus@gmail.com>
2014-10-26 22:45:19 +0400

    Bad player move

--- Assets/Scripts/GameScene/GameEngine.cs
+++ Assets/Scripts/GameScene/GameEngine.cs
@@ -5,10 +5,13 @@ using System;
 
 public class GameEngine : MonoBehaviour {
 
-	public GameObject[,]  Walls;
+	static public GameObject[,]  Walls;
+	public Camera SceneCam;
 	
+	public GameObject Player;
+
 	void Start () {
-	
+
 		var Filename = Menu.NamesArr[Menu.selGridInt].ToString() + ".txt";
 		var TextArrayFromFile = new StreamReader(Application.dataPath + "/levels/" + Filename);	
 		var  LevelArray = TextArrayFromFile.ReadToEnd();
@@ -21,24 +24,28 @@ public class GameEngine : MonoBehaviour {
 		var PlaneLevel = GameObject.CreatePrimitive (PrimitiveType.Plane) as GameObject;
 		PlaneLevel.transform.localScale = new Vector3 (Menu.CubeSize* Menu.CubeNumX / 10 , 1, Menu.CubeSize * Menu.CubeNumY / 10);
 		PlaneLevel.transform.rotation = Quaternion.Euler(-90,transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-		
+		PlaneLevel.transform.position = new Vector3(0,0,8);
 		for (var i = 0; i< Menu.CubeNumX; i ++) {
 					for (var j = 0; j < Menu.CubeNumY; j++) {
 						if(  LevelArray[ArrayCount] != '0') {
-								Walls [i, j] = GameObject.CreatePrimitive (PrimitiveType.Cube) as GameObject;
-								Walls [i, j].name = "Wall";
-								Walls [i, j].transform.renderer.material.color = Color.white;
-								Walls [i, j].transform.position = new Vector3 (((Menu.CubeNumX * Menu.CubeSize) / -2 + Menu.CubeSize / 2) + i * Menu.CubeSize, ((Menu.CubeNumY*Menu.CubeSize) / -2 + Menu.CubeSize / 2) + j * Menu.CubeSize, -1);
-								Walls [i, j].transform.localScale = new Vector3 (Menu.CubeSize, Menu.CubeSize, Menu.CubeSize);
-								
+
+								// 1 - wall 2-Ghost Spawn 3-Player Spawn
 								if ( LevelArray[ArrayCount] == '1' ) {
-										Walls [i, j].transform.renderer.material.color = Color.green;
+									Walls [i, j] = GameObject.CreatePrimitive (PrimitiveType.Cube) as GameObject;
+									Walls [i, j].name = "Wall";
+									Walls [i, j].transform.renderer.material.color = Color.green;
+									Walls [i, j].transform.position = new Vector3 (((Menu.CubeNumX * Menu.CubeSize) / -2 + Menu.CubeSize / 2) + i * Menu.CubeSize, ((Menu.CubeNumY*Menu.CubeSize) / -2 + Menu.CubeSize / 2) + j * Menu.CubeSize, -1);
+									Walls [i, j].transform.localScale = new Vector3 (Menu.CubeSize, Menu.CubeSize, Menu.CubeSize);
 								} 
 								if (LevelArray[ArrayCount] == '2' ) {
-										Walls [i, j].transform.renderer.material.color = Color.red;
+
 								}
 								if (LevelArray[ArrayCount] == '3' ) {
-										Walls [i, j].transform.renderer.material.color = Color.yellow;
+										//SceneCam.transform.position  =transform.position = new Vector3 (((Menu.CubeNumX * Menu.CubeSize) / -2 + Menu.CubeSize / 2) + i * Menu.CubeSize, ((Menu.CubeNumY*Menu.CubeSize) / -2 + Menu.CubeSize / 2) + j * Menu.CubeSize - 60, -60);
+										Player.transform.position  = transform.position = new Vector3 (((Menu.CubeNumX * Menu.CubeSize) / -2 + Menu.CubeSize / 2) + i * Menu.CubeSize, ((Menu.CubeNumY*Menu.CubeSize) / -2 + Menu.CubeSize / 2) + j * Menu.CubeSize, -1);
+										Player.transform.localScale = new Vector3 (Menu.CubeSize, Menu.CubeSize, Menu.CubeSize);
+										PlayerClass.NumAtX = i;
+										PlayerClass.NumAtY = j	;
 								}
 							}
 								ArrayCount++;
---
+++ Assets/Scripts/GameScene/PlayerClass.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+
+	// Use this for initialization
+	static public int NumAtX = 0;
+	static 	public int NumAtY = 0;
+
+	public GameObject Player;
+
+	public float Deltax = 0.0f;
+	public float Deltay = 0.0f;
+
+	public int Napr = 1;
+
+	public float PlayerSpeed = 1f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+	// 1 - down, 2 - up , 3 - right 4 - left;
+	void Update () {
+		if (Input.GetKeyDown ("down")) {
+			Napr = 1;
+		}
+		if (Input.GetKeyDown ("up")) {
+			Napr = 2;
+		}
+		if (Input.GetKeyDown ("right")) {
+			Napr = 3;
+		}
+		if (Input.GetKeyDown ("left")) {
+			Napr = 4;
+		}
+
+		if( Mathf.Abs(Deltay) >= Menu.CubeSize){
+			if(Deltay>0){
+				NumAtY+=1;
+			} else {
+				NumAtY-=1;
+			}
+			Deltay = 0.0f;
+			print(NumAtY);
+		}
+		if( Mathf.Abs(Deltax) >= Menu.CubeSize){
+			if(Deltax>0){
+				NumAtX+=1;
+			} else {
+				NumAtX-=1;
+			}
+			Deltax = 0.0f;
+		}
+
+
+		if(Napr == 1 && ( GameEngine.Walls[NumAtX,NumAtY-1] == null ) ) {
+			Player.transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y- PlayerSpeed,Player.transform.position.z);
+			Deltay-= PlayerSpeed;
+		}
+		if(Napr == 2 && ( GameEngine.Walls[NumAtX,NumAtY+1] == null ) ) {
+			Player.transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y+ PlayerSpeed,Player.transform.position.z);
+			Deltay+= PlayerSpeed;
+		}
+		if(Napr == 3  && ( GameEngine.Walls[NumAtX+1,NumAtY] == null) ) {
+			Player.transform.position = new Vector3(Player.transform.position.x+ PlayerSpeed,Player.transform.position.y,Player.transform.position.z);
+			Deltax+= PlayerSpeed;
+		}
+		if(Napr == 4  && ( GameEngine.Walls[NumAtX-1,NumAtY] == null) ) {
+			Player.transform.position = new Vector3(Player.transform.position.x- PlayerSpeed,Player.transform.position.y,Player.transform.position.z);
+			Deltax-= PlayerSpeed;
+		}
+		
+	}
+}
